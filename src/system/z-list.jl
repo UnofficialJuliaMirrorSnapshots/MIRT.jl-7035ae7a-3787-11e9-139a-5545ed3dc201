71501b7a592eedb0853257f97fa5ef8dc3e68dfa
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-06-30 03:43:18 +0000

    Snapshot of branch master taken on 2019-06-29T23:43:17.153-04:00 from "https://github.com/UnofficialJuliaMirror/MIRT.jl-7035ae7a-3787-11e9-139a-5545ed3dc201"

--- src/system/z-list.jl
+++ src/system/z-list.jl
@@ -1,2 +1,7 @@
-include("lm_getindex.jl")
-include("lm-hcat.jl")
+# system/z-list,jl
+
+include("linear-map/block.jl")
+export block_lm
+
+include("linear-map/getindex.jl")
+include("linear-map/hcat.jl")
--- src/utility/z-list.jl
+++ src/utility/z-list.jl
@@ -1,7 +1,17 @@
+# utility/z-list.jl
+
 include("downsample.jl")
 export downsample1
 export downsample2
 export downsample3
+export downsample
 
 include("map_many.jl")
 export map_many
+
+include("mask.jl")
+export mask_or
+export mask_outline
+export embed
+export maskit
+
