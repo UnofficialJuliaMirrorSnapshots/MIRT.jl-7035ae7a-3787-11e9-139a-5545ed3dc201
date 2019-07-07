21f50f66bb3c6d97be140fa86c2e234e90f0a2c3
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-07 03:42:50 +0000

    Snapshot of branch master taken on 2019-07-06T23:42:49.694-04:00 from "https://github.com/UnofficialJuliaMirror/MIRT.jl-7035ae7a-3787-11e9-139a-5545ed3dc201"

--- src/fbp/z-list.jl
+++ src/fbp/z-list.jl
@@ -4,6 +4,14 @@ include("image_geom.jl")
 export image_geom
 export MIRT_image_geom
 
+include("sino_geom.jl")
+export sino_geom
+export MIRT_sino_geom
+
+
+include("cuboid_im.jl") # must be after image_geom.jl
+export cuboid_im
+
 include("ellipse_im.jl")
 export ellipse_im
 
--- src/fbp/z-test.jl
+++ src/fbp/z-test.jl
@@ -2,9 +2,11 @@
 
 using Test
 
+@test cuboid_im(:test)
 @test ellipse_im(:test)
 @test embed(:test)
 @test image_geom_add_unitv(:test)
 @test image_geom(:test)
 @test rect_im(:test)
 @test rotate2d(:test)
+@test sino_geom(:test)
